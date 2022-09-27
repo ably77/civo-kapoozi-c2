7885e110fc65b1483644a1379980e462eb317718
Alex <alexbrucely@gmail.com>
2022-09-27 12:48:59 -0700

    move cert-manager stuff to wave-2

--- environment/wave-1-clusterconfig/init.sh
+++ environment/wave-1-clusterconfig/init.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-cluster_context="cluster2"
 
 echo "wave description:"
 echo "namespaces, configmaps, secrets"
-
-# create cert-manager namespace without istio injection label
-kubectl create ns cert-manager --context ${cluster_context}
-
 sleep 5
