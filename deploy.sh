7c1240d8f992d7bd8fdfb20333111c1661887b64
Alex <alexbrucely@gmail.com>
2022-09-28 22:58:12 -0700

    improve for loop sort ordering

--- deploy.sh
+++ deploy.sh
@@ -14,19 +14,12 @@ github_username=${5:-ably77}
 repo_name=${6:-civo-kapoozi-c2}
 target_branch=${7:-HEAD}
 
+
 # check to see if defined contexts exist
 if [[ $(kubectl config get-contexts | grep ${cluster_context}) == "" ]] || [[ $(kubectl config get-contexts | grep ${mgmt_context}) == "" ]]; then
   echo "Check Failed: ${cluster_context} context does not exist. Please check to see if you have the clusters available"
   echo "Run 'kubectl config get-contexts' to see currently available contexts. If the clusters are available, please make sure that they are named correctly. Default is ${cluster_context}"
   exit 1;
-fi
-
-# check to see if gloo mesh version variable was passed through, if not prompt for it
-if [[ ${gloo_mesh_version} == "" ]]
-  then
-    # provide license key
-    echo "Please provide the gloo-mesh agent version to use:"
-    read gloo_mesh_version
 fi
 
 # check to see if environment overlay variable was passed through, if not prompt for it
@@ -46,7 +39,7 @@ cd ..
 ./tools/wait-for-rollout.sh deployment argocd-server argocd 20 ${cluster_context}
 
 # deploy app of app waves
-for i in $(ls -l environment/ | grep -v ^total | awk '{print $9}'); do 
+for i in $(ls environment | sort -n); do 
   echo "starting ${i}"
   # run init script if it exists
   [[ -f "environment/${i}/init.sh" ]] && ./environment/${i}/init.sh ${i} ${environment_overlay} ${cluster_context} ${github_username} ${repo_name} ${target_branch}
